b563e8b548816f99d0dd1ad2b6ca5c959e4d36e2
Akhil Kodati <akhilkodati@Akhils-MacBook-Pro.local>
2024-07-27 18:10:34 +0530

    first commit

---
+++ build/lib/nse_live_stocks/main.py
@@ -0,0 +1,31 @@
+import requests
+
+class Nse():
+    def __init__(self):
+        self.url = "https://www.nseindia.com/api/quote-equity?symbol={}"
+        self.session = requests.Session()
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+        self.session.get('https://www.nseindia.com',
+                         headers=self.headers, verify=True)
+        self.response = None
+
+    def get_current_price(self, symbol):
+        self.response = self.session.get(self.url.format(symbol), headers=self.headers, verify=True)
+        if self.response.status_code == 200:
+            if self.response.json().get('message') != "TypeError: Cannot read property 'length' of undefined":
+                return {"error":False,"nse_symbol":symbol,"current_value":str(self.response.json()['priceInfo']['lastPrice']) ,"date": self.response.json()['preOpenMarket']['lastUpdateTime']}
+            return {"error":True,"message":"Stock Not Found/Invalid Input."}
+        raise Exception(
+                f"Not Found status code: {self.response.status_code}")
+
+    def get_stock_info(self, symbol):
+        self.response = self.session.get(self.url.format(symbol), headers=self.headers, verify=True)
+        if self.response.status_code == 200:
+            return self.response.json()
+        raise Exception(
+                f"Not Found status code: {self.response.status_code}")
+
+
+
+""" Developed by Akhil kodati https://github.com/akhilkodati """
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as fileobj:
+        description = fileobj.read()
+
+setup(
+    name='nse_live_stocks',
+    version='0.3',
+    author="Akhil Kodati",
+    author_email="akhilkodati@yahoo.com",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.23.0'
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
